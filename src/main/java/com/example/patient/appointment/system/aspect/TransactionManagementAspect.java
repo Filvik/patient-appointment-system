a386b1aa07688dad0d08e202e522d7d4315404af
Filvik <filvik@bk.ru>
2024-02-19 20:22:20 +0200

    fix

--- src/main/java/com/example/patient/appointment/system/aspect/TransactionManagementAspect.java
+++ src/main/java/com/example/patient/appointment/system/aspect/TransactionManagementAspect.java
@@ -1,33 +1,42 @@
 package com.example.patient.appointment.system.aspect;
 
+import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@Slf4j
 public class TransactionManagementAspect {
 
-//    @Before("execution(* com.example.patient.appointment.system.service.AppointmentService.*(..))")
+    //    @Before("execution(* com.example.patient.appointment.system.service.AppointmentService.*(..))")
     @Before("@annotation(org.springframework.transaction.annotation.Transactional)")
     public void beforeTransaction(JoinPoint joinPoint) {
-        System.out.println("Before transaction: " + joinPoint.getSignature().getName());
+        log.info("Before transaction: " + joinPoint.getSignature().getName());
     }
 
     @Around("@annotation(org.springframework.transaction.annotation.Transactional)")
     public Object manageTransaction(ProceedingJoinPoint joinPoint) throws Throwable {
-        System.out.println("Transaction started");
+        log.info("Transaction started");
         Object result = joinPoint.proceed();
-        System.out.println("Transaction committed with result: " + result);
+        log.info("Transaction committed with result: " + result);
         return result;
     }
 
     @After("@annotation(org.springframework.transaction.annotation.Transactional)")
     public void afterTransaction(JoinPoint joinPoint) {
-        System.out.println("After transaction: " + joinPoint.getSignature().getName());
+        log.info("After transaction: " + joinPoint.getSignature().getName());
+    }
+
+    @AfterReturning(pointcut = "execution(* com.example.patient.appointment.system.service.*.*(..))", returning = "result")
+    public void logAfterReturning(JoinPoint joinPoint, Object result) {
+        log.info("Method " + joinPoint.getSignature().getName() + " completed with result: " + result);
+    }
+
+    @AfterThrowing(pointcut = "execution(* com.example.patient.appointment.system.service.*.*(..))", throwing = "e")
+    public void logAfterThrowing(JoinPoint joinPoint, Throwable e) {
+        log.error("Exception in method: " + joinPoint.getSignature().getName() + " with message: " + e.getMessage());
     }
 }
